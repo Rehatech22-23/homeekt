1e31f52a89a78d101033940e27f850f78288fe1f
sebastian <1sebastian1@online.de>
2022-12-07 23:35:36 +0100

    next Test publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.21"
+    id("maven-publish")
 }
 
 group = "de.rehatech"
@@ -21,4 +22,13 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+publishing{
+    publications {
+        create<MavenPublication>("maven")
+        {
+            from(components["java"])
+        }
+    }
 }
